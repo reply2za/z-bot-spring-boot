695639dac882be488071be34fc892be2d4b6e9c2
Zain <reply2zain@gmail.com>
2023-05-22 17:36:37 -0400

    test: add tests for help cmd

---
+++ src/test/java/hoursofza/commands/client/HelpTest.java
@@ -0,0 +1,70 @@
+package hoursofza.commands.client;
+
+import hoursofza.utils.DiscordUtils;
+import hoursofza.utils.MessageEventLocal;
+import hoursofza.utils.MockMessageSetup;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class HelpTest {
+
+    @Mock
+    private DiscordUtils discordUtils;
+
+    @Mock
+    private MessageEventLocal messageEvent;
+
+    private Help help;
+
+    private MockMessageSetup mockMessageSetup;
+
+    private AutoCloseable autoCloseable;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        help = new Help(discordUtils);
+        mockMessageSetup = new MockMessageSetup();
+        when(messageEvent.getMessage()).thenReturn(mockMessageSetup.message);
+        mockMessageSetup.setupMocks();
+    }
+
+    @AfterEach
+    public void breakDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    public void testExecute() {
+        ArgumentCaptor<MessageCreateData> messageCaptor = ArgumentCaptor.forClass(MessageCreateData.class);
+
+        help.execute(messageEvent);
+
+        verify(messageEvent, times(1)).getMessage();
+        verify(mockMessageSetup.message, times(1)).getAuthor();
+        verify(mockMessageSetup.user, times(1)).getAsMention();
+        verify(mockMessageSetup.message, times(1)).getChannel();
+        verify(mockMessageSetup.channel, times(1)).sendMessage(messageCaptor.capture());
+        verify(mockMessageSetup.messageAction, times(1)).queue();
+        verify(mockMessageSetup.message, times(1)).addReaction(any(Emoji.class));
+        verify(mockMessageSetup.reactionAction, times(1)).queue();
+        verify(discordUtils, times(1)).awaitReaction(any(Message.class), anyInt(), any(), any(), any());
+
+        MessageCreateData capturedMessage = messageCaptor.getValue();
+        Assertions.assertTrue(capturedMessage.getEmbeds().get(0).getDescription().contains("I'm a simple"));
+    }
+}
---
+++ src/test/java/hoursofza/utils/MockMessageSetup.java
@@ -0,0 +1,51 @@
+package hoursofza.utils;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.requests.restaction.AuditableRestAction;
+import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
+import net.dv8tion.jda.api.utils.messages.MessageCreateData;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+public class MockMessageSetup {
+
+    @Mock
+    public Message message;
+
+    @Mock
+    public User user;
+
+    @Mock
+    public MessageChannelUnion channel;
+
+    @Mock
+    public MessageCreateAction messageAction;
+
+    @Mock
+    public RestAction<Void> reactionAction;
+
+    @Mock
+    public AuditableRestAction<Void> auditableRestAction;
+
+    public MockMessageSetup() throws Exception {
+            AutoCloseable autoCloseable = MockitoAnnotations.openMocks(this);
+            setupMocks();
+            autoCloseable.close();
+    }
+
+    public void setupMocks() {
+        when(message.getAuthor()).thenReturn(user);
+        when(user.getAsMention()).thenReturn("@user");
+        when(message.getChannel()).thenReturn(channel);
+        when(channel.sendMessage(any(MessageCreateData.class))).thenReturn(messageAction);
+        when(message.addReaction(any(Emoji.class))).thenReturn(reactionAction);
+        when(message.removeReaction(any(Emoji.class), any(User.class))).thenReturn(auditableRestAction);
+    }
+}
