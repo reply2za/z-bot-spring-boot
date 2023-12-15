fdda94be82b318502923431e3b651cb5a8ad23c1
Zain <reply2zain@gmail.com>
2023-12-14 21:35:36 -0500

    script: update message on build successs/failure

--- pm2/start.bash
+++ pm2/start.bash
@@ -1,4 +1,11 @@
-git stash && git pull && mvn clean install
+git stash && git pull
+
+
+if mvn clean install; then
+  echo "updated"
+else
+  echo "ensure you are running this command from the root of the project"
+fi
 
 pm2 delete z-bot-spring-boot;
 
